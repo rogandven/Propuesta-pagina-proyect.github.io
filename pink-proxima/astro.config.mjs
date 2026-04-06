baf23bdf9d2310afff15c929ec477bb0981e3e43
Roger Andrés Venegas Opazo <roger.venegas2301@alumnos.ubiobio.cl>
2026-04-06 14:32:52 -0400

    Installed Astro Icons

--- pink-proxima/astro.config.mjs
+++ pink-proxima/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sebastianpinto2424.github.io',
   base: '/Propuesta-pagina-proyect.github.io/',
+  integrations: [icon()],
 });
